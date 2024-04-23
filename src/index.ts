83e9a217efaa15626ac77a6adde1f7fa5e4eb32d
LiangYuxuan <lyx@lyx.es>
2024-04-23 10:53:11 +0800

    feat: builder

---
+++ src/index.ts
@@ -0,0 +1,154 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-bitwise */
+/* eslint-disable no-console */
+
+import assert from 'node:assert';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { CASCClient, WDCReader, DBDParser } from '@rhyster/wow-casc-dbc';
+
+import { latestVersion } from './client.ts';
+
+const client = new CASCClient('us', latestVersion.product, latestVersion.version);
+await client.init();
+
+console.log(new Date().toISOString(), '[INFO]: Loading remote TACT keys');
+await client.loadRemoteTACTKeys();
+console.log(new Date().toISOString(), '[INFO]: Loaded remote TACT keys');
+
+const fileDataID2CKey = client.preload?.rootFile.fileDataID2CKey;
+assert(fileDataID2CKey, 'Failed to get root file and fileDataID2CKey');
+
+const loadDB2 = async (fileDataID: number) => {
+    const cKeys = client.getContentKeysByFileDataID(fileDataID);
+    assert(cKeys, `No cKeys found for fileDataID ${fileDataID.toString()}`);
+
+    const cKey = cKeys
+        // eslint-disable-next-line no-bitwise
+        .find((data) => !!(data.localeFlags & CASCClient.LocaleFlags.enUS));
+    assert(cKey, `No cKey found for fileDataID ${fileDataID.toString()} in enUS`);
+
+    const data = await client.getFileByContentKey(cKey.cKey, true);
+    const reader = new WDCReader(data.buffer, data.type === 'partial' ? data.blocks : undefined);
+    const parser = await DBDParser.parse(reader);
+
+    return parser;
+};
+
+console.log(new Date().toISOString(), '[INFO]: Loading DB2 files');
+const [mapXArt, artTile, overlay, overlayTile] = await Promise.all([
+    loadDB2(1957217), // dbfilesclient/uimapxmapart.db2
+    loadDB2(1957210), // dbfilesclient/uimaparttile.db2
+    loadDB2(1134579), // dbfilesclient/worldmapoverlay.db2
+    loadDB2(1957212), // dbfilesclient/worldmapoverlaytile.db2
+]);
+console.log(new Date().toISOString(), '[INFO]: Loaded DB2 files');
+
+console.log(new Date().toISOString(), '[INFO]: Parsing DB2 files');
+const art2Map = new Map(
+    mapXArt
+        .getAllIDs()
+        .map((id) => {
+            const row = mapXArt.getRowData(id);
+            const artID = row?.UiMapArtID;
+            const mapID = row?.UiMapID;
+
+            assert(typeof artID === 'number' && typeof mapID === 'number');
+
+            return [artID, mapID];
+        }),
+);
+
+interface TileFileInfo {
+    fileDataID: number,
+    source: 'ArtTile' | 'OverlayTile',
+    cKey: string,
+}
+
+const tileFiles: TileFileInfo[] = [];
+const handleMapFile = (fileDataID: number, artID: number, source: 'ArtTile' | 'OverlayTile') => {
+    if (artID > 1800 && fileDataID > 0 && art2Map.has(artID)) {
+        const cKeys = fileDataID2CKey.get(fileDataID);
+        assert(cKeys, `Failed to get content keys of ${source} fileDataID ${fileDataID.toString()}`);
+
+        const cKey = cKeys.find((v) => !!(v.localeFlags & CASCClient.LocaleFlags.enUS));
+        const zhCN = cKeys.find((v) => !!(v.localeFlags & CASCClient.LocaleFlags.zhCN));
+
+        if (cKey && !zhCN) {
+            tileFiles.push({
+                fileDataID, source, cKey: cKey.cKey,
+            });
+        }
+    }
+};
+
+artTile
+    .getAllIDs()
+    .forEach((id) => {
+        const row = artTile.getRowData(id);
+        const fileDataID = row?.FileDataID;
+        const artID = row?.UiMapArtID;
+
+        assert(typeof fileDataID === 'number' && typeof artID === 'number');
+
+        handleMapFile(fileDataID, artID, 'ArtTile');
+    });
+
+const overlay2Art = new Map(
+    overlay
+        .getAllIDs()
+        .map((id) => {
+            const row = overlay.getRowData(id);
+            const artID = row?.UiMapArtID;
+
+            assert(typeof artID === 'number');
+
+            return [id, artID];
+        }),
+);
+
+overlayTile
+    .getAllIDs()
+    .forEach((id) => {
+        const row = overlayTile.getRowData(id);
+        const fileDataID = row?.FileDataID;
+        const overlayID = row?.WorldMapOverlayID;
+
+        assert(typeof fileDataID === 'number' && typeof overlayID === 'number');
+
+        const artID = overlay2Art.get(overlayID);
+
+        assert(artID);
+
+        handleMapFile(fileDataID, artID, 'OverlayTile');
+    });
+console.log(new Date().toISOString(), '[INFO]: Parsed DB2 files');
+
+console.log(new Date().toISOString(), '[INFO]: Generating tile files list');
+const tileIDs = tileFiles
+    .map(({ fileDataID }) => `[${fileDataID.toString()}] = "Interface/AddOns/TestServerWorldMap/tiles/${fileDataID.toString()}.blp"`)
+    .join(',\n    ');
+
+const dataFile = path.join(fileURLToPath(import.meta.url), '..', '..', 'TestServerWorldMap', 'Data.lua');
+await fs.writeFile(
+    dataFile,
+    `local _, addon = ...\n\naddon.tiles = {\n    ${tileIDs},\n}\n`,
+);
+console.log(new Date().toISOString(), '[INFO]: Generated tile files list');
+
+console.log(new Date().toISOString(), '[INFO]: Updating tile files');
+const tilesDir = path.join(fileURLToPath(import.meta.url), '..', '..', 'TestServerWorldMap', 'tiles');
+await fs.rm(tilesDir, { recursive: true }).catch(() => {});
+await fs.mkdir(tilesDir);
+
+// eslint-disable-next-line no-restricted-syntax
+for (const { fileDataID, cKey } of tileFiles) {
+    console.log(new Date().toISOString(), `[INFO]: Download ${fileDataID.toString()}.blp`);
+
+    const res = await client.getFileByContentKey(cKey);
+
+    const file = path.join(tilesDir, `${fileDataID.toString()}.blp`);
+    await fs.writeFile(file, res.buffer);
+}
